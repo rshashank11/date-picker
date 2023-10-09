fea58b9fe7f6b1aea3f192e7500086e5598e17f1
rshashank11 <shashankyadav2000@gmail.com>
2023-10-09 21:32:35 +0530

    Added components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./App.css"
+import DatePicker from "./components/DatePicker"
+
+const App = () => {
+  return (
+    <div>
+      <DatePicker />
+    </div>
+  )
+}
+
+export default App
